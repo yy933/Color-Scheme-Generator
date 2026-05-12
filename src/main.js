d68aed783757842b4c4ee921e3c915a0b6fc6240
yy933 <116449560+yy933@users.noreply.github.com>
2026-05-12 11:57:29 +0800

    feat: render scheme options with provided choices by JS

--- src/main.js
+++ src/main.js
@@ -3,3 +3,28 @@ import { initTheme } from "./theme.js";
 
 // initialize theme
 initTheme();
+
+const modeOptions = [
+  "monochrome",
+  "monochrome-dark",
+  "monochrome-light",
+  "analogic",
+  "complement",
+  "analogic-complement",
+  "triad",
+  "quad",
+];
+const modeSelect = document.getElementById("mode-select");
+function renderModeOptions() {
+  const modeOptionsHtml = modeOptions
+    .map((option) => {
+      let displayOption = option.replace(/-/g, " ");
+      displayOption =
+        displayOption.charAt(0).toUpperCase() + displayOption.slice(1);
+      return `<option value="${option}">${displayOption}</option>`;
+    })
+    .join("");
+  modeSelect.innerHTML = modeOptionsHtml;
+}
+
+renderModeOptions();
